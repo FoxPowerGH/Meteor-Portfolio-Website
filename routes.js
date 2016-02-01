d9da356c6d666319f49ba9b3933fc3348f069831
Jorge Rodriguez <jorge.rodriguez08@icloud.com>
2016-02-01 00:06:15 -0500

    Basic Stuff
    1. Layout Improvement
    2. Minor improvement Navbar

--- routes.js
+++ routes.js
@@ -1,9 +1,40 @@
+ // set up the main template the the router will use to build pages
 Router.configure({
   layoutTemplate: 'layout'
 });
-Router.route('/', function () {
-  this.render('home');
-});
-Router.route('/news', function () {
-  this.render('news');
-});
+
+
+  // Prevent unauthorized access
+  Router.onBeforeAction(function () {
+    if (!Meteor.user()) {
+      this.render("navbar", {to:"header"});
+      this.render("AccessDenied");
+    } else
+    {
+      this.next();
+    }
+  });
+  
+
+// specify the top level route, the page users see when they arrive at the site
+	Router.route('/', function () {
+		this.render("navbar", {to:"header"});
+		this.render("home", {to:"main"});
+ 		this.render("navbar-bottom", {to:"footer"});  
+	});
+
+
+	Router.route('/news', function () {
+		this.render("navbar", {to:"header"});
+		this.render("news", {to:"main"});
+ 		this.render("navbar-bottom", {to:"footer"});  
+	});
+
+
+	Router.route('/blogIndex', function () {
+		this.render("navbar", {to:"header"});
+		this.render("blog", {to:"main"});
+ 		this.render("navbar-bottom", {to:"footer"});  
+	});
+
+
